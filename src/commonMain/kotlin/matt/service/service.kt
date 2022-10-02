c468e806516156be8a6d940ebebe07657fdee0eb
mgroth0 <mgroth49@gmail.com>
2022-10-01 23:31:53 -0400

    autocommit

--- src/commonMain/kotlin/matt/service/service.kt
+++ src/commonMain/kotlin/matt/service/service.kt
@@ -4,3 +4,16 @@ package matt.service
 /*maybe these should just be called interfaces... services are different things related to servers and web and port programming*/
 /*but I need to give them a better name than interface... maybe InterfaceProper ...or maybe API ... idk ... good thing I have different words. Give them different meanings.*/
 interface MattService
+
+abstract class ServiceHub<S: MattService> {
+  private var service: S? = null
+
+  @Synchronized
+  fun install(s: S) {
+	require(service == null)
+	service = s
+  }
+
+  fun get() = service!!
+
+}
