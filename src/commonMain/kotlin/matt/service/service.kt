aaf6b7b3ac0950a4bf649e497bcb95be633bb9ec
mgroth0 <mgroth49@gmail.com>
2022-10-08 20:43:18 -0400

    autocommit

--- src/commonMain/kotlin/matt/service/service.kt
+++ src/commonMain/kotlin/matt/service/service.kt
@@ -1,9 +1,11 @@
 package matt.service
 
+import matt.model.idea.ServiceIdea
+
 
 /*maybe these should just be called interfaces... services are different things related to servers and web and port programming*/
 /*but I need to give them a better name than interface... maybe InterfaceProper ...or maybe API ... idk ... good thing I have different words. Give them different meanings.*/
-interface MattService
+interface MattService: ServiceIdea
 
 abstract class ServiceHub<S: MattService> {
   private var service: S? = null
