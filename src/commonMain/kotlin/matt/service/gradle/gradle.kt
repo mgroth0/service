9e57097cf3974532a41ce5aaaf7e0fb501b91787
mgroth0 <mgroth49@gmail.com>
2022-10-10 11:08:19 -0400

    autocommit

--- src/commonMain/kotlin/matt/service/gradle/gradle.kt
+++ src/commonMain/kotlin/matt/service/gradle/gradle.kt
@@ -1,13 +1,13 @@
 package matt.service.gradle
 
-import matt.model.mod.ModType
+import matt.model.mod.AbsoluteMod
 import matt.service.MattService
 
 interface GradleService: MattService {
   fun project(path: String): GradleProjectService
 }
 
-interface GradleProjectService: MattService, ModType {
+interface GradleProjectService: MattService, AbsoluteMod {
   val path: String
   val subProjects: List<GradleProjectService>
 }
