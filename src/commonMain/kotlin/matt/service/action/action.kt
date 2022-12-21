c312f18c37dae5d669055d03d6ce05cc2751d9fc
mgroth0 <mgroth49@gmail.com>
2022-12-21 16:17:58 -0500

    autocommit

--- src/commonMain/kotlin/matt/service/action/action.kt
+++ src/commonMain/kotlin/matt/service/action/action.kt
@@ -1,8 +1,10 @@
 package matt.service.action
 
 import matt.model.code.idea.ServiceIdea
+import matt.model.data.dir.YesOrNo
 
 interface ActionAbilitiesService: ServiceIdea {
   fun input(prompt: String): String
   fun confirm(s: String): Boolean
+  fun yesOrNo(s: String): YesOrNo
 }
