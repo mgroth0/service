bac0e3e1c3ed83c0ed021ac7491e482455ae38dc
mgroth0 <mgroth49@gmail.com>
2023-02-12 02:12:11 -0500

    autocommit

--- src/commonMain/kotlin/matt/service/action/action.kt
+++ src/commonMain/kotlin/matt/service/action/action.kt
@@ -5,6 +5,7 @@ import matt.model.data.dir.YesOrNo
 
 interface ActionAbilitiesService: ServiceIdea {
   fun input(prompt: String): String
+  fun openFile(prompt: String): String?
   fun confirm(s: String): Boolean
   fun yesOrNo(s: String): YesOrNo
   fun <E: Enum<E>> enum(enumOptions: Array<E>, prompt: String): E
