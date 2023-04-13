a700f330b53399d3df4e9bfd51850a48f6368648
mgroth0 <mgroth49@gmail.com>
2023-04-13 17:34:42 -0400

    changed src/commonMain/kotlin/matt/service/service.kt

--- src/commonMain/kotlin/matt/service/service.kt
+++ src/commonMain/kotlin/matt/service/service.kt
@@ -6,17 +6,23 @@ import matt.model.code.idea.ServiceIdea
 
 /*maybe these should just be called interfaces... services are different things related to servers and web and port programming*/
 /*but I need to give them a better name than interface... maybe InterfaceProper ...or maybe API ... idk ... good thing I have different words. Give them different meanings.*/
-interface MattService: ServiceIdea
+interface MattService : ServiceIdea
 
-abstract class ServiceHub<S: MattService> {
-  private var service: S? = null
+/*alternative name idea: ServiceRegistry*/
+abstract class ServiceHub<S : MattService> {
+    private var service: S? = null
 
-  @OnlySynchronizedOnJvm
-  fun install(s: S) {
-	require(service == null)
-	service = s
-  }
+    @OnlySynchronizedOnJvm
+    fun install(s: S) {
+        require(service == null)
+        service = s
+    }
 
-  fun get() = service ?: error("service was not installed for ${this::class.simpleName}")
+   /* @OnlySynchronizedOnJvm
+    fun clear() {
+        service = null
+    }*/
+
+    fun get() = service ?: error("service was not installed for ${this::class.simpleName}")
 
 }
