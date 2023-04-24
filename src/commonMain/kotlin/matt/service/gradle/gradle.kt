7d50e5757f3c2ca8bfbd5ccf2806c99746395762
mgroth0 <mgroth49@gmail.com>
2023-04-24 13:41:59 -0400

    changed src/commonMain/kotlin/matt/service/gradle/gradle.kt

--- src/commonMain/kotlin/matt/service/gradle/gradle.kt
+++ src/commonMain/kotlin/matt/service/gradle/gradle.kt
@@ -1,17 +1,26 @@
 package matt.service.gradle
 
 import matt.model.code.mod.AbsoluteMod
+import matt.model.code.mod.RelativeToKMod
 import matt.service.MattService
 
-interface GradleService: MattService {
-  fun project(path: String): GradleProjectService
+interface GradleService : MattService {
+    val rootDir: String
+    fun project(path: String): GradleProjectService
+    val allKSubProjects: List<RelativeToKMod>
 }
 
-object NoGradleService: GradleService {
-  override fun project(path: String) = error("no gradle service")
+object NoGradleService : GradleService {
+    override val rootDir: String
+        get() = error("no gradle service")
+
+    override fun project(path: String) = error("no gradle service")
+
+    override val allKSubProjects: List<RelativeToKMod>
+        get() = error("no gradle service")
 }
 
-interface GradleProjectService: MattService, AbsoluteMod {
-  val path: String
-  val subProjects: List<GradleProjectService>
+interface GradleProjectService : MattService, AbsoluteMod {
+    val path: String
+    val subProjects: List<GradleProjectService>
 }
