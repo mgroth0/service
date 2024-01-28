502a14274b1442718e5bc60b35b045d211bff2f2
mgroth0 <mgroth49@gmail.com>
2024-01-27 19:04:28 -0500

    changed 2 files

--- src/commonMain/kotlin/matt/service/frames/frames.kt
+++ src/commonMain/kotlin/matt/service/frames/frames.kt
@@ -1,12 +1,12 @@
 package matt.service.frames
 
-import matt.lang.model.file.FilePath
+import matt.lang.model.file.AnyResolvableFilePath
 import matt.service.MattService
 
 interface MFrameGrabber : MattService {
     fun extractVideoFrames(
-        video: FilePath,
-        outputFolder: FilePath,
+        video: AnyResolvableFilePath,
+        outputFolder: AnyResolvableFilePath,
         framesToExtract: List<Int>
     )
 }
--- src/jvmMain/kotlin/matt/service/service.kt
+++ src/jvmMain/kotlin/matt/service/service.kt
@@ -6,5 +6,3 @@ import kotlin.jvm.optionals.getOrNull
 
 inline fun <reified T : Any> loadService() = ServiceLoader.load(T::class.java).findFirst().get()
 inline fun <reified T : Any> loadServiceOrNull() = ServiceLoader.load(T::class.java).findFirst().getOrNull()
-
-object YesIUseService
